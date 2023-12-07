51b258fd3df9d6baff53c3bd04f8829468744341
CodeWithPartha <153088732+CodeWithPartha@users.noreply.github.com>
2023-12-07 04:08:56 -0800

    C#

---
+++ array.cs
@@ -0,0 +1,19 @@
+using System;
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        /*int[] num = {1,2,3,3,4,5,6,7,8};*/
+        /*for (int i = 0; i < num.Length; i++)  // for loop
+        {
+        Console.WriteLine(num[i]); 
+        }*/
+        
+        /*String [] arr = {"Mango", "Apple", "Carrot"};
+        arr[0] = "Banana";
+        foreach (String arrs in arr )
+        {
+            Console.WriteLine(arrs);
+        }*/
+    }
+}
